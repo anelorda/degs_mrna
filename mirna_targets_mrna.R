531f8309cdea1d22d808ed4fb0f795f7e6fb24f0
Anel Ordabayeva <ordabayevaa99@gmail.com>
2024-08-16 15:34:32 +0500

    added pngs

--- mirna_targets_mrna.R
+++ mirna_targets_mrna.R
@@ -1,8 +1,9 @@
 library(VennDiagram)
 library(dplyr)
 
-# Define the function to create the Venn diagram
-create_venn_diagram <- function(dataframes, labels) {
+
+# Define the function to create the Venn diagram with title and save it
+create_venn_diagram <- function(dataframes, labels, title, output_filename) {
   # Ensure the number of dataframes and labels match
   if (length(dataframes) != length(labels)) {
     stop("The number of data frames and labels must be the same.")
@@ -19,12 +20,15 @@ create_venn_diagram <- function(dataframes, labels) {
   venn.plot <- venn.diagram(
     x = symbol_lists,
     category.names = labels,
-    filename = NULL,  # Use NULL to keep it in R
+    main = title,
+    filename = output_filename,  # Save to file
     output = TRUE
   )
   
-  # Plot the Venn diagram
-  grid.draw(venn.plot)
+  # Plot the Venn diagram if the filename is NULL
+  if (is.null(output_filename)) {
+    grid.draw(venn.plot)
+  }
 }
 
 # Load your data
@@ -47,16 +51,21 @@ mirtarbase_4_down$target_symbol <- NULL
 # Create the Venn diagram for the three stages
 create_venn_diagram(
   dataframes = list(mirtarbase_2_down, group2_downregulated),
-  labels = c("miRNA targets (mirtarbase)", "mRNA")
+  labels = c("miRNA targets (mirtarbase)", "mRNA"),
+  title = "miRNA targets found by mirtarbase and DEGs from mRNA-seq for stage2",
+  output_filename = "deseq_mrna/2mirnatargets_vs_DEGs.png"
 )
 
 create_venn_diagram(
   dataframes = list(mirtarbase_3_down, group3_downregulated),
-  labels = c("miRNA targets (mirtarbase)", "mRNA")
+  labels = c("miRNA targets (mirtarbase)", "mRNA"),
+  title = "miRNA targets found by mirtarbase and DEGs from mRNA-seq for stage3",
+  output_filename = "deseq_mrna/3mirnatargets_vs_DEGs.png"
 )
 
 create_venn_diagram(
   dataframes = list(mirtarbase_4_down, group4_downregulated),
-  labels = c("miRNA targets (mirtarbase)", "mRNA")
+  labels = c("miRNA targets (mirtarbase)", "mRNA"),
+  title = "miRNA targets found by mirtarbase and DEGs from mRNA-seq for stage4",
+  output_filename = "deseq_mrna/4mirnatargets_vs_DEGs.png"
 )
-
