335e3a007ed90c76c3c8fa19141550ff263317fc
Anel Ordabayeva <ordabayevaa99@gmail.com>
2024-08-19 13:16:45 +0500

    added png of venn diagrams for overlapping genes between mirna target and identified degs from mirna

--- mirna_targets_mrna.R
+++ mirna_targets_mrna.R
@@ -37,16 +37,19 @@ create_venn_diagram <- function(dataframes, labels, title, output_filename) {
 mirna_stage2_down <- read.table("Downloads/multimir_results_stage2_down.csv", header = TRUE, sep = ",")
 mirna_stage3_down <- read.table("Downloads/multimir_results_stage3_down.csv", header = TRUE, sep = ",")
 mirna_stage4_down <- read.table("Downloads/multimir_results_stage4_down.csv", header = TRUE, sep = ",")
+mirna_stage2_up <- read.table("Downloads/multimir_results_stage2_up.csv", header = TRUE, sep = ",")
 
 # Filter for "mirtarbase" entries
 mirtarbase_2_down <- mirna_stage2_down %>% filter(database == "mirtarbase")
 mirtarbase_3_down <- mirna_stage3_down %>% filter(database == "mirtarbase")
 mirtarbase_4_down <- mirna_stage4_down %>% filter(database == "mirtarbase")
+mirtarbase_2_up <- mirna_stage2_up %>% filter(database == "mirtarbase")
 
 # Filter for "mirtarbase" entries
 tarbase_2_down <- mirna_stage2_down %>% filter(database == "tarbase")
 tarbase_3_down <- mirna_stage3_down %>% filter(database == "tarbase")
 tarbase_4_down <- mirna_stage4_down %>% filter(database == "tarbase")
+tarbase_2_up <- mirna_stage2_up %>% filter(database == "tarbase")
 
 mirtarbase_2_down$hgnc_symbol <- mirtarbase_2_down$target_symbol
 mirtarbase_2_down$target_symbol <- NULL
@@ -54,6 +57,8 @@ mirtarbase_3_down$hgnc_symbol <- mirtarbase_3_down$target_symbol
 mirtarbase_3_down$target_symbol <- NULL
 mirtarbase_4_down$hgnc_symbol <- mirtarbase_4_down$target_symbol
 mirtarbase_4_down$target_symbol <- NULL
+mirtarbase_2_up$hgnc_symbol <- mirtarbase_2_up$target_symbol
+mirtarbase_2_up$target_symbol <- NULL
 
 # Rename target_symbol to hgnc_symbol and remove target_symbol column for targetscan
 tarbase_2_down$hgnc_symbol <- tarbase_2_down$target_symbol
@@ -62,11 +67,14 @@ tarbase_3_down$hgnc_symbol <- tarbase_3_down$target_symbol
 tarbase_3_down$target_symbol <- NULL
 tarbase_4_down$hgnc_symbol <- tarbase_4_down$target_symbol
 tarbase_4_down$target_symbol <- NULL
+tarbase_2_up$hgnc_symbol <- tarbase_2_up$target_symbol
+tarbase_2_up$target_symbol <- NULL
 
 # Load downregulated gene lists
 group2_downregulated <- read.table("deseq_mrna/downregulated_group2.csv", header = TRUE, sep = ",")
 group3_downregulated <- read.table("deseq_mrna/downregulated_group3.csv", header = TRUE, sep = ",")
 group4_downregulated <- read.table("deseq_mrna/downregulated_group4.csv", header = TRUE, sep = ",")
+gtoup2_upregulated <- read.table("deseq_mrna/upregulated_group2.csv", header = TRUE, sep = ",")
 
 # Create the Venn diagram for the three stages with mirtarbase
 create_venn_diagram(
